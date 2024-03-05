6ebecd2fff8bdd4effc695c592be11f4a048c13d
Pav85 <pawel.werbowy@gmail.com>
2024-03-05 20:03:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -54,6 +54,12 @@ const locations = [
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters.",
   },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster.",
+  },
 ];
 
 // initialize buttons
@@ -144,7 +150,10 @@ function fightDragon() {
   goFight();
 }
 
-function goFight() {}
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+}
 
 function attack() {}
 
