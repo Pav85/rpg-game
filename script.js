d97781a7b9bf771cfc069c766ccb1260b503626f
Pav85 <pawel.werbowy@gmail.com>
2024-03-06 08:42:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -268,6 +268,15 @@ function pick(guess) {
   while (numbers.length < 10) {
     numbers.push(Math.floor(Math.random() * 11));
   }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 5; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  }
 }
 
 function pickTwo() {
