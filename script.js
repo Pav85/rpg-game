88ec45367de7532d0005dfc8d1891aa6b54e881f
Pav85 <pawel.werbowy@gmail.com>
2024-03-06 08:17:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -204,8 +204,9 @@ function attack() {
       defeatMonster();
     }
   }
-  if (Math.random() <= 0.1) {
+  if (Math.random() <= 0.1 && inventory.length !== 1) {
     text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeapon--;
   }
 }
 
